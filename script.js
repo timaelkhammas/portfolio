1edcdf4c2d7f419ff79a30ef5a40eee53c587313
timaelkhammas <99289981+timaelkhammas@users.noreply.github.com>
2024-07-21 00:09:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+const pages = [
+    {
+        content: "EKA GD MA Display Library #4 'What We Did With The Budget' with Laura Martens, presenting a collection of publications and objects acquired from the Ghent Art Book Fair.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-1',
+        isPortrait: false
+    },
+    {
+        content: "EKA GD MA Display Library #4 'What We Did With The Budget' with Laura Martens, presenting a collection of publications and objects acquired from the Ghent Art Book Fair.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-2',
+        isPortrait: false 
+    },
+    {
+        content: " Double roomsheet/poster design for the Craft Studies MA exhibition 'Objects or Things' at the Estonian Academy of Arts.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-3',
+        isPortrait: false
+    },
+    {
+        content: "Double roomsheet/poster design for the Craft Studies MA exhibition 'Objects or Things' at the Estonian Academy of Arts.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-4',
+        isPortrait: false
+    },
+    {
+        content: "In-context lecture tour 'I Know Enough To Ask For Water' exploring Ülemiste lake's mythology. Part of Laura Pappa's course Work in Context.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-5',
+        isPortrait: false
+    },
+    {
+        content: "Workshop 'Myth within Memory' exploring myth as a mediator of memory through a series of drawing exercises. Part of Laura Pappa's course Work in Context.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-6',
+        isPortrait: false
+    },
+    {
+        content: "Workshop 'Myth within Memory' exploring myth as a mediator of memory through a series of drawing exercises. Part of Laura Pappa's course Work in Context.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-7',
+        isPortrait: false
+    },
+    {
+        content: "Issue #1 of ta[kh]zine, an independent research and publishing project dedicated to archives of political publishing practices in the context of Morocco.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-8',
+        isPortrait: false
+    },
+    {
+        content: "Issue #1 of ta[kh]zine, an independent research and publishing project dedicated to archives of political publishing practices in the context of Morocco.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-9',
+        isPortrait: false
+    },
+    {
+        content: "Workshop 'Hammer & Chisel' organized with Laura Martens as part of the Stand-in School for Graphic Design program in Berlin, Germany.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-10',
+        isPortrait: false
+    },
+    {
+        content: "Workshop 'Hammer & Chisel' organized with Laura Martens as part of the Stand-in School for Graphic Design program in Berlin, Germany.",
+        topLeftText: `<a href="mailto:timaelkhammas@gmail.com">Fatima-Ezzahra El Khammas</a>`,
+        topRightText: ` <a href="https://www.instagram.com/timaelkhammas/" target="_blank">About</a>`,
+        className: 'page-11',
+        isPortrait: false
+    },
+];
+
+
+let currentPage = 0;
+
+function updateContent() {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = `<p>${pages[currentPage].content}</p>`;
+    document.body.className = pages[currentPage].className;
+    
+    if (pages[currentPage].isPortrait) {
+        document.body.classList.add('portrait');
+    } else {
+        document.body.classList.remove('portrait');
+    }
+
+    const topLeftText = document.querySelector('.top-left-text');
+    if (pages[currentPage].topLeftText) {
+        topLeftText.innerHTML = pages[currentPage].topLeftText.replace(/\n/g, '<br>');
+        topLeftText.style.display = 'block';
+    } else {
+        topLeftText.innerHTML = "";
+        topLeftText.style.display = 'none';
+    }
+
+    const topRightText = document.querySelector('.top-right-text');
+    if (pages[currentPage].topRightText) {
+        topRightText.innerHTML = pages[currentPage].topRightText.replace(/\n/g, '<br>');
+        topRightText.style.display = 'block';
+    } else {
+        topRightText.innerHTML = "";
+        topRightText.style.display = 'none';
+    }
+}
+
+function navigate(direction) {
+    if (direction === 'next' && currentPage < pages.length - 1) {
+        currentPage++;
+    } else if (direction === 'prev' && currentPage > 0) {
+        currentPage--;
+    }
+    updateContent();
+}
+
+document.querySelector('.right').addEventListener('click', () => navigate('next'));
+document.querySelector('.left').addEventListener('click', () => navigate('prev'));
+
+updateContent();
