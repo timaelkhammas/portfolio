013528c66c4669ff15b6dc44f3f8b8e62b3fbb47
timaelkhammas <99289981+timaelkhammas@users.noreply.github.com>
2024-07-23 17:40:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1}
     if (slideIndex < 1) {slideIndex = slides.length}
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 1000); // Change image every 3 seconds
+    setTimeout(showSlides, 2000); // Change image every 3 seconds
 }
 
 function plusSlides(n) {
